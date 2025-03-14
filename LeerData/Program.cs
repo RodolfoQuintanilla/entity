4f91970f2468bdf354d46451b3548e8c88c01ef7
Rodolfo <rqm@outlook.es>
2025-03-13 19:51:13 -0600

    configuracion de entity

---
+++ LeerData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeerData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a la aplicación de lectura de datos");
+        }
+    }
+}
